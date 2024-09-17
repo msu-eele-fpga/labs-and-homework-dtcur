1ae016da3bd302a8d1fedb2d58e8243b5e2b1321
drewc <drew.currie@student.montana.edu>
2024-09-17 12:55:19 -0600

    Completed testbench test cases

--- sim/async-conditioner/one_pulse_tb.vhd
+++ sim/async-conditioner/one_pulse_tb.vhd
@@ -33,14 +33,51 @@ architecture testbench of one_pulse_tb is
 	signal rst_tb	: std_ulogic := '1';
 	signal pulse_tb : std_ulogic := '0';
 
-		
+
 begin
+	-- Create the one pulse duv	
+	duv : component one_pulse
+		port map (
+		clk => clk_tb,
+		rst => rst_tb,
+		input => input_tb,
+		pulse => pulse_tb
+		);
+		
 	-- Create TB clock signal
 	clk_tb <= not clk_tb after CLK_PERIOD / 2;
 	
 	stimuli_and_checker : process is
 	begin
-			
+		-- Start low for a while holding reset for 10 clock cycles
+		wait_for_clock_edges(clk_tb, 10);
+		-- Wait fo clock peroid then change
+		wait for CLK_PERIOD / 2;
+		
+		rst_tb <= '0';
+		
+		-- Test no input and reset off to make sure no pulse
+		wait_for_clock_edges(clk_tb, 5);
+
+
+		-- Set Input high for two clock pulses and ensure it maps to pulse for one pulse
+		input_tb <= '1';
+		wait_for_clock_edges(clk_tb, 2);
+		input_tb <= '0';
+		wait_for_clock_edges(clk_tb, 3);
+
+		--Set input low in the middle of a clock pulse
+		wait_for_clock_edge(clk_tb);
+		wait for CLK_PERIOD / 4;
+		input_tb <= '1';
+		wait_for_clock_edges(clk_tb, 2);
+		wait for CLK_PERIOD / 4;
+		input_tb <= '0';
+		wait_for_clock_edges(clk_tb, 5);
+		
+		std.env.finish;
+
+	end process;
 end architecture;
 
 
