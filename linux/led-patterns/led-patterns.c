4b08de4731ad7707fe861c6fe032530c143a8cd4
Drew Currie <drew.currie@student.montana.edu>
2024-12-07 18:56:54 -0700

    Completed driver

--- linux/led-patterns/led-patterns.c
+++ linux/led-patterns/led-patterns.c
@@ -14,6 +14,7 @@
 #include <linux/miscdevice.h>
 #include <linux/types.h>
 #include <linux/fs.h>
+#include <linux/kstrtox.h>
 
 // Memory offsets for the LED control registers
 #define BASE_ADDRESS 0xFF200000u
@@ -269,6 +270,165 @@ static const struct of_device_id led_patterns_of_match[] = {
     {}};
 MODULE_DEVICE_TABLE(of, led_patterns_of_match);
 
+/**
+ * led_reg_show() - Return the led_reg value to user-space via sysfs.
+ * @dev: Device structure for the led_patterns component. This
+ * device struct is embedded in the led_patterns' platform
+ * device struct.
+ * @attr: Unused.
+ * @buf: Buffer that gets returned to user-space.
+ *
+ * Return: The number of bytes read.
+ */
+static ssize_t led_reg_show(struct device *dev,
+                            struct device_attribute *attr, char *buf)
+{
+    u8 led_reg;
+    struct led_patterns_dev *priv = dev_get_drvdata(dev);
+    led_reg = ioread32(priv->led_reg);
+    return scnprintf(buf, PAGE_SIZE, "%u\n", led_reg);
+}
+
+/**
+ * led_reg_store() - Store the led_reg value.
+ * @dev: Device structure for the led_patterns component. This
+ * device struct is embedded in the led_patterns' platform
+ * device struct.
+ * @attr: Unused.
+ * @buf: Buffer that contains the led_reg value being written.
+ * @size: The number of bytes being written.
+ *
+ * Return: The number of bytes stored.
+ */
+static ssize_t led_reg_store(struct device *dev,
+                             struct device_attribute *attr, const char *buf, size_t size)
+{
+    u8 led_reg;
+    int ret;
+    struct led_patterns_dev *priv = dev_get_drvdata(dev);
+    // Parse the string we received as a u8
+    // See https://elixir.bootlin.com/linux/latest/source/lib/kstrtox.c#L289
+    ret = kstrtou8(buf, 0, &led_reg);
+    if (ret < 0)
+    {
+        return ret;
+    }
+    iowrite32(led_reg, priv->led_reg);
+    // Write was successful, so we return the number of bytes we wrote.
+    return size;
+}
+
+/**
+ * hps_led_control_show() - Return the hps_led_control value
+ * to user-space via sysfs.
+ * @dev: Device structure for the led_patterns component. This
+ * device struct is embedded in the led_patterns' device struct.
+ * @attr: Unused.
+ * @buf: Buffer that gets returned to user-space.
+ *
+ * Return: The number of bytes read.
+ */
+static ssize_t hps_led_control_show(struct device *dev,
+                                    struct device_attribute *attr, char *buf)
+{
+    bool hps_control;
+    // Get the private led_patterns data out of the dev struct
+    struct led_patterns_dev *priv = dev_get_drvdata(dev);
+    hps_control = ioread32(priv->hps_led_control);
+    return scnprintf(buf, PAGE_SIZE, "%u\n", hps_control);
+}
+/**
+ * hps_led_control_store() - Store the hps_led_control value.
+ * @dev: Device structure for the led_patterns component. This
+ * device struct is embedded in the led_patterns'
+ * platform device struct.
+ * @attr: Unused.
+ * @buf: Buffer that contains the hps_led_control value being written.
+ * @size: The number of bytes being written.
+ *
+ * Return: The number of bytes stored.
+ */
+static ssize_t hps_led_control_store(struct device *dev,
+                                     struct device_attribute *attr, const char *buf, size_t size)
+{
+    bool hps_control;
+    int ret;
+    struct led_patterns_dev *priv = dev_get_drvdata(dev);
+    // Parse the string we received as a bool
+    // See https://elixir.bootlin.com/linux/latest/source/lib/kstrtox.c#L289
+    ret = kstrtobool(buf, &hps_control);
+    if (ret < 0)
+    {
+        // kstrtobool returned an error
+        return ret;
+    }
+    iowrite32(hps_control, priv->hps_led_control);
+    // Write was successful, so we return the number of bytes we wrote.
+    return size;
+}
+
+/**
+ * base_period_show() - Return the base_period value to user-space via sysfs.
+ * @dev: Device structure for the led_patterns component. This
+ * device struct is embedded in the led_patterns' platform
+ * device struct.
+ * @attr: Unused.
+ * @buf: Buffer that gets returned to user-space.
+ *
+ * Return: The number of bytes read.
+ */
+static ssize_t base_period_show(struct device *dev,
+                                struct device_attribute *attr, char *buf)
+{
+    u8 base_period;
+    struct led_patterns_dev *priv = dev_get_drvdata(dev);
+    base_period = ioread32(priv->base_period);
+    return scnprintf(buf, PAGE_SIZE, "%u\n", base_period);
+}
+/**
+ * base_period_store() - Store the base_period value.
+ * @dev: Device structure for the led_patterns component. This
+ * device struct is embedded in the led_patterns' platform
+ * device struct.
+ * @attr: Unused.
+ * @buf: Buffer that contains the base_period value being written.
+ * @size: The number of bytes being written.
+ *
+ * Return: The number of bytes stored.
+ */
+static ssize_t base_period_store(struct device *dev,
+                                 struct device_attribute *attr, const char *buf, size_t size)
+{
+    u8 base_period;
+    int ret;
+    struct led_patterns_dev *priv = dev_get_drvdata(dev);
+    // Parse the string we received as a u8
+    // See https://elixir.bootlin.com/linux/latest/source/lib/kstrtox.c#L289
+    ret = kstrtou8(buf, 0, &base_period);
+    if (ret < 0)
+    {
+        // kstrtou8 returned an error
+        return ret;
+    }
+    iowrite32(base_period, priv->base_period);
+    // Write was successful, so we return the number of bytes we wrote.
+    return size;
+}
+
+// Define sysfs attributes
+static DEVICE_ATTR_RW(hps_led_control);
+static DEVICE_ATTR_RW(base_period);
+static DEVICE_ATTR_RW(led_reg);
+// Create an attribute group so the device core can
+// export the attributes for us.
+static struct attribute *led_patterns_attrs[] = {
+    &dev_attr_hps_led_control.attr,
+    &dev_attr_base_period.attr,
+    &dev_attr_led_reg.attr,
+    NULL,
+};
+ATTRIBUTE_GROUPS(led_patterns);
+
 /*
  * struct led_patterns_driver - Platform driver struct for the led_patterns driver
  * @probe: Function that's called when a device is found
@@ -285,6 +445,7 @@ static struct platform_driver led_patterns_driver = {
         .owner = THIS_MODULE,
         .name = "led_patterns",
         .of_match_table = led_patterns_of_match,
+        .dev_groups = led_patterns_groups,
     },
 };
 
